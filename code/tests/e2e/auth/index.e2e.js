7ec88ef1c5c9ba07aae62ebc96435afd091ff45e
JFcoDiaz <jfcodiaz@gmail.com>
2024-06-29 13:22:23 -0600

    🔧 Fixed authentication tests ✅

--- code/tests/e2e/auth/index.e2e.js
+++ code/tests/e2e/auth/index.e2e.js
@@ -1,13 +1,14 @@
-const UserService = require('../../../services/user.service.js');
 const { e2e } = require('../../utils/e2e.js');
+const { ADMIN } = require('../../utils/users.js');
 const loginTest = require('./login.js');
-
-const userService = new UserService();
 
 e2e({
   title: 'Test for auth /api/v1/auth',
   beforeAll: async (suite) => {
-    suite.user = await userService.findOne(1);
+    suite.loadUser(ADMIN);
+  },
+  beforeEach: async (suite) => {
+    suite.asGuest();
   },
   tests: (suite) => {
     loginTest(suite);
--- code/tests/e2e/auth/login.js
+++ code/tests/e2e/auth/login.js
@@ -1,12 +1,18 @@
 const { decodeToken } = require("../../../services/jwt/decode-token");
+const { ADMIN } = require("../../utils/users");
 
 module.exports = (suite) => {
+  const url = '/api/v1/auth/login';
   test('POST /login', async () => {
-    const { user } = suite;
-    const { body, statusCode } = await suite.api.post('/api/v1/auth/login').send({
-      "email": user.email,
-      "password": process.env.INITIAL_PASS
+    await suite.loadUser(ADMIN);
+    const user = suite.getUser(ADMIN);
+    const { body, statusCode } = await suite.post(url, {
+      data: {
+        "email": user.email,
+        "password": process.env.INITIAL_PASS
+      }
     });
+
     expect(statusCode).toBe(200);
     expect(body.token).toBeTruthy();
     expect(body.user.email).toEqual(user.email);
@@ -16,24 +22,30 @@ module.exports = (suite) => {
   });
 
   test('POST /login without data', async () => {
-    const { statusCode, body, text } = await suite.api.post('/api/v1/auth/login').send();
+    const { statusCode, body, text } = await suite.post(url);
+
     failedExpects(400, 'Bad Request', statusCode, body, text);
   })
 
   test('POST /login with invalid user', async () => {
-    const { statusCode, body, text } = await suite.api.post('/api/v1/auth/login').send({
+    const { statusCode, body, text } = await suite.post(url, {
+      data: {
       "email": 'bad@userfake.com',
       "password": process.env.INITIAL_PASS
+      }
     });
+
     failedExpects(401, /Unauthorized/, statusCode, body, text);
   });
 
   test('POST /login with valid password', async () => {
-    const { user } =  suite;
-    const { statusCode, body, text } = await suite.api.post('/api/v1/auth/login').send({
-      "email": user.email,
-      "password": 'invalidPAssword'
+    const { statusCode, body, text } = await suite.post(url, {
+      data : {
+        "email": suite.getUser(ADMIN).email,
+        "password": 'invalidPAssword'
+      }
     });
+
     failedExpects(401, /Unauthorized/, statusCode, body, text);
   })
 
