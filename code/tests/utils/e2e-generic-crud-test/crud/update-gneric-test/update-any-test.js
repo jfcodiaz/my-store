83c88b7dedebe6534b90a2288f98116c797471a3
JFcoDiaz <jfcodiaz@gmail.com>
2024-07-06 18:16:33 -0600

    🧪 [#1] Added test to update product 🚀

---
+++ code/tests/utils/e2e-generic-crud-test/crud/update-gneric-test/update-any-test.js
@@ -0,0 +1,36 @@
+const expectToBeNotSameArrayProps = require('../../../expects/to-be-not-same-array-props');
+const expectToBeSameArrayProp = require('../../../expects/to-be-same-array-prop');
+
+module.exports = ({
+  suite,
+  update,
+  buildData,
+  getParams,
+  getArgumets,
+  EntityRepository
+} = {}) => {
+  update.usersCanUpdateAny.forEach(as => {
+    const repostory = new EntityRepository();
+    test(`Update ${repostory.model.modelName} as  ${as}`, async () => {
+      const [entity, data] = await Promise.all([
+        repostory.findRandom(),
+        buildData()
+      ]);
+      const keys = Object.keys(data);
+      suite
+        .setEndpoint(
+          'entity',
+          await getParams(entity),
+          await getArgumets(entity)
+        )
+        .as('admin');
+      const { statusCode, body } = await suite.patch({ data });
+      const updateEntity = await repostory.findOne(entity.id);
+
+      expect(statusCode).toBe(200);
+      expectToBeNotSameArrayProps(keys, entity, updateEntity);
+      expectToBeSameArrayProp(keys, data, body);
+      expectToBeSameArrayProp(keys, data, updateEntity.dataValues);
+    });
+  });
+};
